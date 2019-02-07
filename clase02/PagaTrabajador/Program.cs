f289122b7ed636d79dd0db8abf7b8d7d23402030
Ivan-Davila <arkangel_ivan@hotmail.com>
2019-02-07 11:27:41 -0600

    clase02

---
+++ clase02/PagaTrabajador/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PagaTrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            int horas;
+            float pagabruta, impuesto,paganeta,paga, tasa=0.1f;
+            //Console.Clear();
+            Console.WriteLine("Programa para calcular la paga de un rabajador");
+            //entrada
+            Console.Write("Nombre del trabajador: "); nombre=Console.ReadLine();
+            Console.Write("Horas Trabajadas    : "); horas=int.Parse(Console.ReadLine());
+            Console.Write("Paga por Hora       : "); paga=int.Parse(Console.ReadLine());
+            
+            pagabruta = horas * paga;
+            impuesto = pagabruta * tasa;
+            paganeta = pagabruta - impuesto;
+            
+            Console.Write($"El trabajador {nombre} trabajo {horas} horas,"); 
+            Console.Write($"con un pago de {paga} por hora");
+            Console.Write($" y recive una paga neta de: {paganeta}, con un impuesto de {impuesto} rebajado de su paga bruta de {pagabruta}"); 
+        }
+    }
+}
---
+++ tarea01/program01/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace program01
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int op,c,sum=0;
+            Console.Clear();
+            if(args.Length==0){
+                Console.WriteLine("Programa que ejemplifica el uso de ciclos y estucturas de control en c#");
+                Console.WriteLine("Debes espicificar la opcion deseada");
+                menu();
+                return 1;
+            }
+
+            op=int.Parse(args[0]);
+            switch(op){
+                case 1: {
+                    c=1;
+                    while(c<=100){
+                        Console.Write($"{c} ");
+                        sum+=c++;
+                    }
+
+                }break;
+                case 2: {
+                    c=100;
+                    while(c>0){
+                        Console.Write($"{c} ");
+                        sum+=c--;
+                    }
+
+                }break;
+                case 3: {
+                    for(int i=50;i<=200;i++){
+                        sum+=1;
+                        Console.Write($"{i}");
+                    }                   
+
+                }break;
+                case 4: {
+                    for(int i=2;i<=100;i+=2){
+                        sum+=1;
+                        Console.Write($"{i}");
+                    }
+                }break;
+                case 5:{
+                    for(int i=99; i>=1;i-=2){
+                        sum+=i;
+                        Console.Write($"{i} ");
+                    }
+                }break;
+                case 6:{
+                    c=272;
+                    while(c>=40){
+                        Console.Write($"{c} ");
+                        sum+=c;
+                        c-=4;
+                    }
+                }break;
+            }
+            Console.WriteLine($"{sum}");
+            return 0;
+        }
+
+        static void menu(){
+            Console.WriteLine("[1] Numeros del 1 al 100 con ciclo while");
+            Console.WriteLine("[2] Numeros del 100 al 1 con ciclo do .. while");
+            Console.WriteLine("[3] ");
+        }
+    }
+
+}
