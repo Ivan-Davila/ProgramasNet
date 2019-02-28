64d6b8261a85438ab3952b575e58bca07715bd2a
Ivan-Davila <arkangel_ivan@hotmail.com>
2019-02-28 10:37:07 -0600

    hola

---
+++ Evaluacion/Parcial1/Redes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
